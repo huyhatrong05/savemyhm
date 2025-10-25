8f2ac54ac8c62aecf70d7fca5842ab4e4ab8d73c
huyhatrong05 <huyhatrong05@gmail.com>
2025-10-25 21:13:47 +0300

    ЛР1.java

---
+++ ЛР1.java
@@ -0,0 +1,10 @@
+//Список на Java
+ArrayList<String> list = new ArrayList<>();
+list.add("Ёжик");
+list.add("Змея");
+
+//Cтек на Java
+MyStack<Integer> stack = new MyStack<>(10).
+stack.push(10);
+stack.push(20);
+stack.push(30);
